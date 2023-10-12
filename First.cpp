cd08e1d5b5f8569cda30dbe35f20c118fc0ed30e
GeorgeThomas2020 <geovinkenash@gmail.com>
2023-10-12 10:40:00 +0530

    added

---
+++ First.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"\n Enter First Number";
+    cin>>a;
+    cout<<"\n Enter second Number ";
+    cin>>b;
+    cout<<endl<<"Sum of "<<a<<"+ "<<b<<" = "<<a+b;
+    return 0;
+}
